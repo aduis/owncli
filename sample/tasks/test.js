6cf157d8596e3f54b4ae32cf8819aa3abd733a25
Markus Dethlefsen <dethlefsen@cleverbridge.com>
2015-05-06 11:58:38 +0200

    added more samples improve validate output a bit

--- sample/tasks/test.js
+++ sample/tasks/test.js
@@ -3,11 +3,19 @@ module.exports = {
 		one: {
 			execute: require('./test-one'),
 			options: {
-				save: { pos: 0, abbr: 's', required: true }
+				save: { abbr: 's', required: true }
 			}
 		},
 		two: {
 			execute: require('./test-two')
-		}
+		},
+        three: {
+            execute: require('./test-three'),
+            options: {
+                verbose: { abbr: 'v' },
+                save: { abbr: 's', required: true },
+                env: { abbr: 'e', required: true, values:['dev','prod'] }
+            }
+        }
 	}
 };
