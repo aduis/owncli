aa31a757189524c11fe2747f0de702bbb4daf6ff
Andre Duis <andre.duis@gmail.com>
2015-05-11 22:07:57 +0200

    more changes

--- sample/tasks/test-one.js
+++ sample/tasks/test-one.js
@@ -6,11 +6,9 @@ module.exports = function(options){
     
         .echo("Here we go!")
         .exec("grunt test")
-        
         .get('version').from('package.json').as('oldVersion')
         .semver('oldVersion').as('newVersion')
         .set('version').in('package.json').to('newVersion')
-        
         .echo("Done!")
         
         .go(function (err, results) {
