ab8ddec22aa3042193bdc17972f1d5fbcb3fc5aa
Andre Duis <andre.duis@gmail.com>
2015-05-11 22:07:25 +0200

    first version of runner interface

--- sample/tasks/test-one.js
+++ sample/tasks/test-one.js
@@ -1,3 +1,20 @@
+var runner = require('../../index.js').runner;
+
 module.exports = function(options){
-	console.log("one");
+    
+    runner(options)
+    
+        .echo("Here we go!")
+        .exec("grunt test")
+        
+        .get('version').from('package.json').as('oldVersion')
+        .semver('oldVersion').as('newVersion')
+        .set('version').in('package.json').to('newVersion')
+        
+        .echo("Done!")
+        
+        .go(function (err, results) {
+            console.log("results", results);
+        });
+      
 };
--- sample/tasks/test-three.js
+++ sample/tasks/test-three.js
@@ -1,3 +1,4 @@
+
 module.exports = function(options){
 	console.log("three");
 };
